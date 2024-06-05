22a76c04f7fb59b45949a4ada374b858e604dabb
JovanDavchev1 <jovandavcev@yahoo.com>
2024-06-05 18:01:27 +0200

    commit 3

---
+++ script.js
@@ -0,0 +1,46 @@
+let content = [
+    {
+        "question": "How many time zones are there in Russia?",
+        "answer_1": "9",
+        "answer_2": "11",
+        "answer_3": "15",
+        "answer_4": "13",
+        "right_answer": 2
+    },
+    {
+        "question": "What country has the most islands in the world?",
+        "answer_1": "Greece",
+        "answer_2": "Germany",
+        "answer_3": "Canada",
+        "answer_4": "Sweden",
+        "right_answer": 4
+    },
+    {
+        "question": "What’s the capital of Canada?",
+        "answer_1": "Winnipeg",
+        "answer_2": "Montreal",
+        "answer_3": "Ottawa",
+        "answer_4": "Vancouver",
+        "right_answer": 3
+    },
+    {
+        "question": "Until 1923, what was the Turkish city of Istanbul called?",
+        "answer_1": "Alexandria",
+        "answer_2": "Constantinople",
+        "answer_3": "Ankara",
+        "answer_4": "Antalya",
+        "right_answer": 2
+    },
+    {
+        "question": "Which language has the most words (according to dictionary entries)?",
+        "answer_1": "English",
+        "answer_2": "German",
+        "answer_3": "Chinese",
+        "answer_4": "Russian",
+        "right_answer": 1
+    }
+];
+
+function render() {
+
+}
